284ba1482540309a046effe6eea75062c7c08cb7
Daniel <danielmacielcorrea@gmail.com>
2025-03-29 12:13:44 -0300

    Exemplo select com where.

--- select_where_pandas.py
+++ select_where_pandas.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 pessoas = pd.read_csv("Pessoas.csv", sep=";",header=0)
 
-pessoas_nome_pais = pessoas[["Nome", "Pais"]]
+pessoas_brasil = pessoas[pessoas["Pais"] == 'Brazil']
 
-print(pessoas_nome_pais)
+print(pessoas_brasil)
+
