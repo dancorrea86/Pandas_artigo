24070788bae0c1424fea445283acce9d223dd4cc
Daniel <danielmacielcorrea@gmail.com>
2025-03-29 15:35:02 -0300

    Exemplo de Update no Pandas.

---
+++ update_pandas.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+pessoas = pd.read_csv("Pessoas.csv", sep=";",header=0)
+
+ppessoas_brasil = pessoas.loc[pessoas['Pais'] == 'Brazil', 'Pais'] = 'Brasil'
+
+pessoas_brasil = pessoas[pessoas["Pais"] == 'Brasil']
+print(pessoas_brasil)
